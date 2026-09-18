ab348a21742b3aa0bff7a8adda64493ebdcd0965
wtzet-eng <wtzet@yahoo.com>
2026-09-18 22:30:11 +0200

    Finish localized report presentation

--- server/reporting/slovakLocalizedReport.ts
+++ server/reporting/slovakLocalizedReport.ts
@@ -326,3 +326,5 @@ export function renderSlovakLocalizedReport(canonical: CanonicalReport): any {
     opportunities: ['Kanonický model zachováva pôvod a stav každého dôkazu.', 'Terénne, pôdne a zdrojové údaje sú zobrazené v jednom spoločnom hodnotení.']
   };
 }
+
+[executed on device: toma (e8359509-e325-4515-b2ff-2da47ff811ad)]
