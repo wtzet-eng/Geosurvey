8eaff7934803b6dbc638e9574bf33900102e4bc6
wtzet-eng <wtzet@yahoo.com>
2026-09-18 10:15:05 +0200

    Show quiet Ko-fi shortcut on all report widths

--- src/components/ReportView.tsx
+++ src/components/ReportView.tsx
@@ -7,7 +7,7 @@ import { ReportViewSwedish } from './ReportViewSwedish';
 import { ReportViewDanish } from './ReportViewDanish';
 import { AIInterpretationPanel } from './AIInterpretationPanel';
 import { SiteReport } from '../types';
-import { SupportLandSurf } from './SupportLandSurf';
+import { FloatingSupportLandSurf, SupportLandSurf } from './SupportLandSurf';
 
 interface ReportViewProps {
   report: SiteReport;
@@ -34,6 +34,7 @@ export const ReportView: React.FC<ReportViewProps> = ({ report, onBack }) => {
 
   return <>
     <AIInterpretationPanel report={report} />
+    <FloatingSupportLandSurf />
     {reportView}
     <SupportLandSurf />
   </>;
