5cc90fee6471e523cc5d3db858ba26a911df02f5
wtzet-eng <wtzet@yahoo.com>
2026-09-18 22:30:57 +0200

    Finish localized report presentation

--- src/components/ReportView.tsx
+++ src/components/ReportView.tsx
@@ -38,3 +38,5 @@ export const ReportView: React.FC<ReportViewProps> = ({ report, onBack }) => {
     <SupportLandSurf language={report.language} />
   </>;
 };
+
+[executed on device: toma (e8359509-e325-4515-b2ff-2da47ff811ad)]
