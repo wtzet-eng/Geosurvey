142a8b2dce812f70e7f4adb835c9e888e805da49
wtzet-eng <wtzet@yahoo.com>
2026-09-18 16:05:52 +0200

    Denmark audit: src/data/countrySupport.ts

--- src/data/countrySupport.ts
+++ src/data/countrySupport.ts
@@ -55,7 +55,7 @@ const SUPPORT: Record<string, CountrySupportProfile> = {
   FR: { countryCode: 'FR', maturity: 'LIMITED', capabilities: { ...NONE, nationalGeology: true, nationalBoreholes: true, nationalValuation: true } },
   SK: { countryCode: 'SK', maturity: 'LIMITED', capabilities: { ...NONE, nationalGeology: true, nationalBoreholes: true, nationalHydrogeology: true, nationalValuation: true } },
   CZ: { countryCode: 'CZ', maturity: 'LIMITED', capabilities: { ...NONE, nationalCadastre: true, nationalGeology: true, nationalBoreholes: true, nationalHydrogeology: true, nationalRadon: true, nationalMining: true } },
-  DK: { countryCode: 'DK', maturity: 'LIMITED', capabilities: { ...NONE, nationalCadastre: true, nationalGeology: true, nationalBoreholes: true, nationalHydrogeology: true, nationalPlanning: true } },
+  DK: { countryCode: 'DK', maturity: 'LIMITED', capabilities: { ...NONE, nationalCadastre: true, nationalBoreholes: true, nationalHydrogeology: true, nationalPlanning: true } },
   SE: { countryCode: 'SE', maturity: 'LIMITED', capabilities: { ...NONE, nationalGeology: true, nationalBoreholes: true, nationalHydrogeology: true } },
   NO: { countryCode: 'NO', maturity: 'LIMITED', capabilities: { ...NONE, nationalCadastre: true, nationalGeology: true, nationalBoreholes: true, nationalRadon: true } },
   AT: valuationOnly('AT'), ES: valuationOnly('ES'), FI: valuationOnly('FI'),
@@ -144,3 +144,5 @@ export function getCountrySupportNotice(countryCode: string, language = 'en'): s
   if (support.maturity === 'LIMITED' && Object.values(support.capabilities).some(Boolean)) return notices[lang].PARTIAL;
   return notices[lang][support.maturity];
 }
+
+[executed on device: toma (e8359509-e325-4515-b2ff-2da47ff811ad)]
